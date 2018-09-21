756dd7275d639c570c4915f06609009cfd25774b
Bruno Henrique <43428674+bmhenrique@users.noreply.github.com>
2018-09-21 08:06:56 -0300

    Add files via upload

---
+++ Exerc_cio_Teste.ino
@@ -0,0 +1,42 @@
+// CURSO IOT NA PRÁTICA ESP8266 - Blink Led 
+
+int LED = D4;
+
+void setup() {
+
+pinMode(D4,OUTPUT);
+pinMode(D1,INPUT_PULLUP); // Inicializa o D1 como entrada PullUp
+}
+
+void loop() {
+
+digitalWrite(LED,LOW);
+delay(1000);   
+digitalWrite(LED,HIGH);
+delay(1000); 
+}
+
+// CURSO IOT NA PRÁTICA ESP8266 - Botão
+
+int led = D4;
+int button = D3;
+
+int buttonState = 0;
+
+void setup() {
+
+pinMode(led,OUTPUT);
+pinMode(button,INPUT_PULLUP); // Inicializa o D1 como entrada PullUp
+}
+
+void loop() {
+
+buttonState = digitalRead(button);
+
+if (buttonState == HIGH) {
+  digitalWrite(led,HIGH);
+}
+else {
+  digitalWrite(led,LOW);
+}
+}

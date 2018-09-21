6c3b65a78049ee77c9ff458e5007d1866a15fb34
Bruno Henrique <43428674+bmhenrique@users.noreply.github.com>
2018-09-21 09:45:34 -0300

    Add files via upload

---
+++ Mem_ria_RAM_e_EEPROM.ino
@@ -0,0 +1,59 @@
+//IOT na prática com o ESP8266
+// RAM
+
+/*1,x = inserir valor x
+  2   = ler memória
+*/
+
+byte memoria;
+int comando;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+
+  if (Serial.available()>0)
+  {
+
+    comando = Serial.parseInt();
+
+    if (comando==1) memoria = Serial.parseInt();
+    if (comando==2) Serial.println(memoria);
+    
+  }
+}
+//EEPROM
+
+#include <EEPROM.h>
+
+byte memoria;
+int comando;
+
+void setup() {
+  Serial.begin(115200);
+  EEPROM.begin(1);
+}
+
+void loop() {
+
+  if (Serial.available()>0)
+  {
+    comando = Serial.parseInt();
+
+    if (comando==1)
+    {
+      memoria = Serial.parseInt();
+      EEPROM.write(0, memoria);
+      EEPROM.commit();
+    }
+    if (comando==2){
+      memoria = EEPROM.read(0);
+      Serial.println(memoria);
+    }
+    
+  }
+
+}
+

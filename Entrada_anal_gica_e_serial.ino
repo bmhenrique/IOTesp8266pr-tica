84b7a4bb509297be2f9b72c83c63cf66e6596343
Bruno Henrique <43428674+bmhenrique@users.noreply.github.com>
2018-09-21 08:55:03 -0300

    Add files via upload

---
+++ Entrada_anal_gica_e_serial.ino
@@ -0,0 +1,21 @@
+//IOT na prática com o ESP8266
+//Entrada analógica e display serial
+
+
+int sensorPin = A0; // Definições dos pinos de Entrada e Saída
+int analogValue = 0; // Definição das variáveis   
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+ 
+  analogValue = analogRead(sensorPin);
+ // Serial.print("Entrada Analógica=");    
+ //Serial.println(analogValue/1023.0*1.0);    
+
+Serial.println(analogValue/1023.0*100.0);
+
+  delay(500);
+}

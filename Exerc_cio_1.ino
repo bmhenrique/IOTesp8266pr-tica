da4b0f2de28166324ce479a7b4bf3cd097be9956
Bruno Henrique <43428674+bmhenrique@users.noreply.github.com>
2018-09-21 08:17:44 -0300

    Add files via upload

---
+++ Exerc_cio_1.ino
@@ -0,0 +1,30 @@
+// CURSO IOT NA PRÁTICA ESP8266 - Exercício proposto
+
+int led = D4;
+int buttonOff = D1;
+int buttonOn = D3;
+
+int buttonStateOff = 0;
+int buttonStateOn = 0;
+
+void setup() {
+
+pinMode(led,OUTPUT);
+pinMode(buttonOff,INPUT_PULLUP); 
+pinMode(buttonOn,INPUT_PULLUP); 
+
+digitalWrite(led,LOW);
+}
+
+void loop() {
+
+buttonStateOff = digitalRead(buttonOff);
+buttonStateOn = digitalRead(buttonOn);
+
+if (buttonStateOn == LOW) {
+  digitalWrite(led,HIGH);
+}
+if (buttonStateOff == LOW){
+  digitalWrite(led,LOW);
+}
+}
